f10525c9d512a7d9c4d275320c9e2dcdf3299f3d
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2023-12-04 19:34:49 +0000

    Función fromEuroToDollar

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-// This is my function that sums two numbers
-const sum = (a,b) => {
-    return a + b
+// Un euro en distintas monedas
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+};
+
+function fromEuroToDollar(amountInEuro) {
+     // Guardo en una variable el valor de la moneda que quiero (posición)
+    const moneda = oneEuroIs["USD"];
+    //Multiplico el valor de la moneda base por la que el usuario introduce
+    const euroInDollar = amountInEuro * moneda;
+    
+    // Con toFixed escojo los decimales a redondear
+    const dollarRounded = euroInDollar.toFixed(3);
+
+    // Como ha sido convertido a string, hago parseFloat para que vuelva a ser 
+    //decimal
+    return parseFloat(dollarRounded);
 }
 
-// Just a console log for ourselves
-console.log(sum(7,3))
-
-// Export the function to be used on other files 
-// (similar to the keyword "export" when using webpack)
-module.exports = { sum };
+// Exportar función
+module.exports = { fromEuroToDollar };
